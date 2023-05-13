ff3fb853124374e6040834ea6d79056f028b7ef5
Farid <hseynli@list.ru>
2023-05-13 11:42:57 +0400

    Added Tasks

---
+++ Lesson12.Static/23.Tasks/Program.cs
@@ -0,0 +1,9 @@
+﻿/*
+ 1. GetFileExtension adlı extension metod yazın. Metod string tipində bir arqument qəbul edəcək. Qəbul edilən arqumentdəki faylın extensionu geri qayıtmalıdır. Heç bir hazır metoddan istifadə etməyin. Misal üçün: text.txt -> txt, myImage.jpeg -> jpeg
+2. GetNumberCountInWord adlı extension metod yazın. Metod qəbul edilən string tipli text-dəki rəqəmlərin sayını geri qaytaracaq. Heç bir hazır metoddan istifadə etməyin.
+3. GetWordCountInText adlı extension metod yazın. Metod qəbul edilən string tipli text-dəki sözlərin sayını geri qaytaracaq.
+4. ValidateUserAge adlı extension metod yazın. Metod qəbul edilən int tipindəki arqumentə əsasən yoxlamalıdır ki, age > 18 və < 100 kiçik olsun. Metod boolean tipində nəticə geriyə qaytarmalıdır.
+ */
+
+
+Console.WriteLine("Hello, World!");
